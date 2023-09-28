ae59d024d6c821c4ff9c49a46d550e2ffb7f4371
malkmusl <56474599+malkmusl@users.noreply.github.com>
2023-09-28 22:57:58 +0200

    Update opengl.rs

--- src/engine/core/renderer/core/opengl.rs
+++ src/engine/core/renderer/core/opengl.rs
@@ -10,6 +10,34 @@ use crate::engine::core::renderer::camera::camera2d;
 
 use super::GameStatus;
 
+/// Creates an OpenGL window for the game with the specified name, width, and height.
+/// This function sets up the necessary event loop, window parameters, OpenGL context,
+/// and initializes game entities such as the player and NPCs. It also handles various
+/// events like window close requests and keyboard input for game control.
+///
+/// # Arguments
+///
+/// * `game_name` - The name of the game.
+/// * `game_width` - The width of the game window.
+/// * `game_height` - The height of the game window.
+///
+/// # Example
+///
+/// ```rust
+/// create_opengl_window("MyGame", 800.0, 600.0);
+/// ```
+///
+/// This function encapsulates the setup and main loop of the game's graphical interface.
+/// It is a crucial part of initializing and running the game.
+
+#[allow(unused_mut)]
+pub fn create_opengl_window(game_name: &str, game_width: f64, game_height: f64) {
+    // ... (rest of the code)
+}
+```
+
+This comment provides a high-level overview of what the function does, its arguments, and its importance within the game's structure.
+
 
 #[allow(unused_mut)]
 pub fn create_opengl_window(game_name: &str, game_width: f64, game_height: f64) {
@@ -85,6 +113,32 @@ pub fn create_opengl_window(game_name: &str, game_width: f64, game_height: f64) 
         }        
     });
 }
+
+/// Updates the content of the game's display. This function is responsible for drawing
+/// the game's elements, including the player and NPCs, onto the display, updating the
+/// background tiles, and finishing the frame.
+///
+/// # Arguments
+///
+/// * `display` - A reference to the glium display where the game is rendered.
+/// * `player` - A mutable reference to the player object to update and draw.
+/// * `npc` - A mutable reference to the NPC (non-player character) object to update and draw.
+///
+/// This function performs the necessary drawing and updating operations, culminating
+/// in finishing the frame, making the changes visible on the game window.
+///
+/// # Example
+///
+/// ```rust
+/// let display = /* initialize your glium display */;
+/// let mut player = /* initialize your player */;
+/// let mut npc = /* initialize your NPC */;
+/// update_content(display, &mut player, &mut npc);
+/// ```
+///
+/// This function is a key part of the game loop, responsible for rendering and updating
+/// the visual elements of the game.
+
 pub fn update_content(display: glium::Display,player: &mut player::Player, npc: &mut npc::NPC){
     let mut frame = display.draw();
     //draw_squareV2(display.clone(), &mut frame);
@@ -94,16 +148,82 @@ pub fn update_content(display: glium::Display,player: &mut player::Player, npc: 
     frame.finish().unwrap();
 }
 
-
+/// Updates the player object and its interaction with the game frame. This function
+/// is responsible for advancing the player's state and handling camera adjustments
+/// to follow the player's position.
+///
+/// # Arguments
+///
+/// * `player` - A mutable reference to the player object to update.
+/// * `frame` - A mutable reference to the game frame where updates take place.
+///
+/// This function calls the `update` method on the player, allowing it to process
+/// its logic and animations. It also ensures that the camera follows the player's
+/// position, keeping them centered within the frame.
+///
+/// # Example
+///
+/// ```rust
+/// let mut player = /* initialize your player */;
+/// let mut frame = /* initialize your game frame */;
+/// update_player(&mut player, &mut frame);
+/// ```
+///
+/// This function plays a crucial role in keeping the player character up-to-date and
+/// managing the in-game camera's behavior.
 
 pub fn update_player(player: &mut player::Player, frame: &mut Frame) {
     player.update(frame);
     camera2d::update_camera_follow_player(player, frame, player.position)
 }
 
+/// Updates the NPC (non-player character) object within the game frame. This function
+/// is responsible for advancing the NPC's state and interactions with the game world.
+///
+/// # Arguments
+///
+/// * `npc` - A mutable reference to the NPC object to update.
+/// * `frame` - A mutable reference to the game frame where updates take place.
+///
+/// This function calls the `update` method on the NPC, allowing it to process its logic,
+/// animations, and interactions with the game world.
+///
+/// # Example
+///
+/// ```rust
+/// let mut npc = /* initialize your NPC */;
+/// let mut frame = /* initialize your game frame */;
+/// update_npc(&mut npc, &mut frame);
+/// ```
+///
+/// This function is essential for keeping the NPC character up-to-date within the game world.
+
 pub fn update_npc(npc: &mut npc::NPC, mut frame: &mut Frame) {
     npc.update(&mut frame);
 }
+
+/// Updates the background tiles within the game frame. This function is responsible for
+/// rendering and updating the background elements, such as tiles or grids, to create the
+/// game's visual environment.
+///
+/// # Arguments
+///
+/// * `display` - A glium display reference where the background tiles are drawn.
+/// * `frame` - A mutable reference to the game frame where updates take place.
+///
+/// This function typically calls specific drawing functions or modules to create the
+/// background tiles or grids, setting the visual backdrop for the game world.
+///
+/// # Example
+///
+/// ```rust
+/// let display = /* initialize your glium display */;
+/// let mut frame = /* initialize your game frame */;
+/// update_background_tiles(display, &mut frame);
+/// ```
+///
+/// This function plays a crucial role in maintaining the visual aspect of the game's
+/// environment.
 
 pub fn update_background_tiles(display: glium::Display, frame: &mut Frame){
     //background_tiles::draw(display.clone(), frame, 10, 10, 0.5);
